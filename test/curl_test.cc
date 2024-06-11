4ddbc7acb44601e23be675612c28bb50f90fa236
Derek Huang <djh458@stern.nyu.edu>
2024-06-11 02:55:47 -0400

    curl_test.cc: add tests for move ctor + move assignment operator for curl_result

--- test/curl_test.cc
+++ test/curl_test.cc
@@ -8,6 +8,7 @@
 #include "pdxka/curl.hh"
 
 #include <string>
+#include <utility>
 
 #include <boost/test/unit_test.hpp>
 #include <curl/curl.h>
@@ -33,11 +34,11 @@ done:
 }
 
 /**
- * Test that the `curl_handle` works properly.
+ * Test that the `curl_handle` works properly for basic construction.
  *
  * Sets some easy options but does not perform anything.
  */
-BOOST_AUTO_TEST_CASE(curl_handle_test)
+BOOST_AUTO_TEST_CASE(curl_handle_basic_test)
 {
   pdxka::curl_handle handle;
   // set data to POST
@@ -46,6 +47,37 @@ BOOST_AUTO_TEST_CASE(curl_handle_test)
   // enable following redirects
   PDXKA_CURL_NOT_OK(curl_easy_setopt(handle, CURLOPT_FOLLOWLOCATION, 1L))
     BOOST_TEST_FAIL("failed to set CURLOPT_FOLLOWLOCATION");
+}
+
+/**
+ * Test that the `curl_handle` works properly for move construction.
+ */
+BOOST_AUTO_TEST_CASE(curl_handle_move_ctor_test)
+{
+  // curl_handle wrapper and raw handle
+  pdxka::curl_handle h1;
+  auto h1h = h1.handle();
+  // perform move construct
+  pdxka::curl_handle h2{std::move(h1)};
+  // h1 should be nullptr, h2 should acquire h1's handle
+  BOOST_TEST_REQUIRE(!h1, "move from h1 failed");
+  BOOST_TEST_REQUIRE(h2 == h1h, "move to h2 failed");
+}
+
+/**
+ * Test that the `curl_handle` works properly for move assignment.
+ */
+BOOST_AUTO_TEST_CASE(curl_handle_move_asgn_test)
+{
+  // curl_handle objects and raw handle
+  pdxka::curl_handle h1;
+  pdxka::curl_handle h2;
+  auto h1h = h1.handle();
+  // perform move assignment
+  h2 = std::move(h1);
+  // h1 nullptr, h2 acquires h1's handle
+  BOOST_TEST_REQUIRE(!h1, "move from h1 failed");
+  BOOST_TEST_REQUIRE(h2 == h1h, "move to h2 failed");
 }
 
 BOOST_AUTO_TEST_SUITE_END()  // libpdxka
