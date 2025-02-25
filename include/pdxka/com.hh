ceb78ad4b407fc1ce4547fc7732c8ca122ad2ddb
Derek Huang <djh458@stern.nyu.edu>
2025-02-25 02:46:19 -0500

    com.hh: remove unused includes

--- include/pdxka/com.hh
+++ include/pdxka/com.hh
@@ -11,11 +11,8 @@
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif  // WIN32_LEAN_AND_MEAN
-
 #include <Windows.h>
-#include <combaseapi.h>
 #include <errhandlingapi.h>
-#include <netlistmgr.h>
 #include <objbase.h>
 
 #include <ios>
